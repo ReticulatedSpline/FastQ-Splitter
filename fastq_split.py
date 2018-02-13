e230b1fb555a84a04d82510c5f0c35cf925aaed6
Ben Carpenter <carpe504@d.umn.edu>
2018-02-13 17:07:52 -0600

    removed str() conversion to deal with escaped character weirdness

--- fastq_split.py
+++ fastq_split.py
@@ -1,8 +1,6 @@
 # Creative Commons - Ben Carpenter 2018
 # Split fastQ files into respective forward and reverse files
 # requires a .fastq file in the same directory as code. Run with `python fastq_split.py`.
-def clean(string):
-    return str(string).replace(" ", "").replace("'", "").replace("\\n", "\n").replace(",", "")
 
 with open("./input.fastq", "r") as infile:
     i = 0
@@ -11,23 +9,25 @@ with open("./input.fastq", "r") as infile:
     # Run through the file opened on line 7 line by line.
     for line in infile:
         if i != 3:
-            chunk.append(str(line))
+            chunk.append(line)
             i += 1
         # Check the first line of the chunk, second to last character (last character is newline)
-        elif str(chunk[0][-2: -1]) == "1":
-            chunk.append(str(line))
+        elif chunk[0][-2: -1] == "1":
+            chunk.append(line)
             print("forward: "),
             print(chunk[0])
             with open("./forward.fastq", "a+") as forward:
-                forward.write(clean(chunk)[1:-1])
+                for cline in chunk:
+                    forward.write(cline)
             chunk = []
             i = 0
         elif str(chunk[0][-2: -1]) == "2":
-            chunk.append(str(line))
+            chunk.append(line)
             print("reverse: "),
             print(chunk[0])
             with open("./reverse.fastq", "a+") as reverse:
-                reverse.write(clean(chunk)[1:-1])
+                for cline in chunk:
+                    reverse.write(cline)
             chunk = []
             i = 0
 print("\nComplete!")
