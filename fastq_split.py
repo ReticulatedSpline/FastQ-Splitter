87364a45081f095d573b2bb9b05b62b29ca960f8
Ben Carpenter <carpe504@d.umn.edu>
2018-02-15 13:06:00 -0600

    file picker and sloppy progress bar

--- fastq_split.py
+++ fastq_split.py
@@ -1,33 +1,78 @@
-# Creative Commons - Ben Carpenter 2018
+# Creative Commons License - Ben Carpenter | 2018 | carpe504@d.umn.edu
 # Split fastQ files into respective forward and reverse files
 # requires a .fastq file in the same directory as code. Run with `python fastq_split.py`.
 
-with open("./input.fastq", "r") as infile:
-    i = 0
-    chunk = []
-    print("Beginning processing...")
-    # Run through the file opened on line 7 line by line.
-    for line in infile:
-        if i != 3:
-            chunk.append(line)
-            i += 1
-        # Check the first line of the chunk, second to last character (last character is newline)
-        elif chunk[0][-2: -1] == "1":
-            chunk.append(line)
-            print("forward: "),
-            print(chunk[0])
-            with open("./forward.fastq", "a+") as forward:
-                for cline in chunk:
-                    forward.write(cline)
-            chunk = []
-            i = 0
-        elif str(chunk[0][-2: -1]) == "2":
-            chunk.append(line)
-            print("reverse: "),
-            print(chunk[0])
-            with open("./reverse.fastq", "a+") as reverse:
-                for cline in chunk:
-                    reverse.write(cline)
-            chunk = []
-            i = 0
-print("\nComplete!")
+import os.path as path
+import sys
+
+input_path = ""
+output_path = ""
+consent = "N"
+linecount = 0
+curline = 0
+
+def print_progress(current_count, total_count, stage):
+    hashes = 70.0 * (float(current_count) / float(total_count))
+    whitespace = 70 - hashes
+    # clear the command line
+    print("\033[H\033[J")
+    # print the progress bar
+    print stage
+    print ("[" + "#" * int(hashes) + " " * int(whitespace) + "]" )
+
+print("|-- Fastq splitter by Ben Jamin'. Press enter to use default values. --|")
+input_path = raw_input("Enter source fastq file name: ")
+if not input_path:
+    input_path = path.realpath("./input.fastq")
+    print (input_path)
+
+output_path = raw_input("Enter destination folder: ")
+if not output_path:
+    output_path = path.realpath("./")
+    print (output_path)
+
+consent = raw_input("Proceed? [Y/n] : ")
+
+if consent.upper() == "Y":
+    stage = "Counting lines..."
+    with open(input_path, "r") as file:
+        for newline in file:
+            linecount += 1
+        print("Lines: " + str(linecount))
+    
+    with open(input_path, "r") as infile:
+        i = 0
+        chunk = []
+
+        # Run through the file opened on line 7 line by line.
+        for line in infile:
+            stage = "Splitting chunks..."
+            print_progress(curline, linecount, stage)
+            curline += 1
+            if i != 3:
+                chunk.append(line)
+                i += 1
+            # Check the first line of the chunk, second to last character (last character is newline)
+            elif chunk[0][-2: -1] == "1":
+                chunk.append(line)
+                with open(output_path + "forward.fastq", "a+") as forward:
+                    for cline in chunk:
+                        forward.write(cline)
+                chunk = []
+                i = 0
+            # Repeat for the reverse "2" direction
+            elif str(chunk[0][-2: -1]) == "2":
+                chunk.append(line)
+                with open(output_path + "reverse.fastq", "a+") as reverse:
+                    for cline in chunk:
+                        reverse.write(cline)
+                chunk = []
+                i = 0
+    # yay
+    stage = "Complete!"
+    print_progress(curline, linecount, stage)
+    print "Results written to " + output_path
+    sys.exit(0)
+else:
+    print("Exiting...")
+    sys.exit(0)
